aca87424ed8647498deeba762992b99832c7c035
jishj-sudo <78237318+jishj-sudo@users.noreply.github.com>
2023-05-05 21:28:48 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Keyboard",
     platforms: [.macOS(.v12), .iOS(.v14)],
     products: [.library(name: "Keyboard", targets: ["Keyboard"])],
-    dependencies: [.package(url: "https://github.com/jishj-sudo/Tonic", .branch(main))],
+    dependencies: [.package(url: "https://github.com/jishj-sudo/Tonic.git", .branch(main))],
     targets: [
         .target(name: "Keyboard", dependencies: ["Tonic"]),
         .testTarget(name: "KeyboardTests", dependencies: ["Keyboard"]),
